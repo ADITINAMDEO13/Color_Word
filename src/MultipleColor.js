9621998e7a1274540870a64dd224b3e4b1897cac
Namdev <adnamdev@deloitte.com>
2021-04-18 19:52:02 +0530

    first commit

---
+++ src/MultipleColor.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import colours from './Colours';
+
+
+
+const MultipleColor = () => {
+    const [color, setColor] = useState(colours);
+    const [search, setSearch] = useState("");
+    const [isClick, setIsClick] = useState(false);
+    const [showHeader, setShowHeader] = useState(true);
+    const [showFooter, setShowFooter] = useState(false);
+
+
+    return (
+        <div >
+            {showHeader === true ?
+                <div className="hdrouter">
+                    <div className="colorheading">Color</div>
+                    <div className="wordheading"><b>WORD</b></div>
+                <div className="header">
+                    
+                    <div className="innerheader"><input type="text" placeholder="Enter color Name" onChange={(e) => setSearch(e.target.value)} />
+                    <button onClick={() => {
+                                setIsClick(true)
+                                setShowHeader(false)
+                                setShowFooter(true)
+                                
+                    }} disabled={search.length<1}>Search</button></div>
+                </div></div>
+                : null
+            }
+
+            <div className="box">
+                {isClick === false ? null :
+                    color.filter((item) => {
+                            if (search === "") {
+                                return null;
+                            }
+                            else if (item.name.toLowerCase().includes(search.toLowerCase())) {
+                                return item;
+                            }
+                    }).map((col) => {
+                            return (
+                                
+                                <div className="boxui">
+                                    <div className="colorbox" style={{backgroundColor:col.hex}}>
+                                        
+                                    </div>
+                                    <div className="hexname">
+                                        {col.hex}
+                                    </div>
+                                </div>
+                            )
+
+                        })
+                    
+                    
+                }
+                
+            </div>
+            <div className="ftrouter">
+            {showFooter === true ?
+                <div className="footer">
+                    <div className="innerfooter">
+                    <input type="text" placeholder="Enter color Name" onChange={(e) => setSearch(e.target.value)} />
+                    <button onClick={() => {
+                        setIsClick(true)
+                        
+                        }}>Search</button>
+                        </div>
+                        
+                </div>
+                : null
+                }
+                </div>
+        </div>
+    );
+}
+
+export default MultipleColor;
